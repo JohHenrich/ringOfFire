a60393695cc6c23b40d8471fc1d836351386f4ed
JohHenrich <j.henrich@gmx.net>
2022-07-25 12:23:52 +0200

    first playable Version

---
+++ app/game/game.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/classes/game';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+  takeCardAnimation = false;
+  currentCard: string = '';
+  game: Game;
+
+  constructor(public dialog: MatDialog) { }
+
+
+  ngOnInit(): void {
+    this.newGame();
+
+  }
+
+  newGame() {
+    this.game = new Game;
+
+  }
+
+  takeCard() {
+    if (!this.takeCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.takeCardAnimation = true;
+
+      this.game.currentPlayer++;
+      this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+
+      setTimeout(() => {
+        this.game.playCards.push(this.currentCard);
+        this.takeCardAnimation = false;
+      }, 1000);
+    }
+  }
+
+
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((name: string) => {
+      if (name && name.length > 0){
+      this.game.players.push(name);
+      }
+    });
+  }
+}
